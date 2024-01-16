94df0deeca248511c6ea6a18423f217f7e5b391f
Tavares <jptavares.dev@gmail.com>
2024-01-15 23:28:21 -0300

    feat: Add global styles and sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 
-import './styles.css'
+import './global.css'
+
+import styles from "./App.module.css"
 
 export function App() {
 
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
 
-      <Post
-        content="Enim aliquip officia veniam incididunt consectetur pariatur excepteur officia nulla."
-        author="Tavares"
-      />
-      <Post
-        content="Cillum ex qui fugiat qui do."
-        author="Dib"
-      />
+        <main>
+          <Post
+            content="Enim aliquip officia veniam incididunt consectetur pariatur excepteur officia nulla."
+            author="Tavares"
+          />
+          <Post
+            content="Cillum ex qui fugiat qui do."
+            author="Dib"
+        />
+        </main>
+      </div>
     </div>
   )
 }
